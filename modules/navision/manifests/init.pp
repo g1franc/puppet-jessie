093db55eb97c3fb86fba9396f92ee4c8b8a3373a
Guillaume FRANCOIS <guillaume.francois55@gmail.com>
2016-06-28 15:41:11 +0000

    Add handling and update of Spring boot application

---
+++ modules/navision/manifests/init.pp
@@ -0,0 +1,44 @@
+class navision ($version)
+{
+
+#	exec { 'etc_init.d_navision':
+#		command => 'ln -s /opt/navision.jar /etc/init.d/navision.jar',
+#		creates => '/etc/init.d/navision.jar',
+#		path => "/usr/bin/:/bin/",
+#		logoutput => true,
+#		before => File ['/etc/apt/sources.list.d/jessie-backport.list'],
+#	}
+
+	file { '/etc/init.d/navision':
+		ensure => link,
+		target => '/opt/navision.jar',
+		mode=> '755',	
+	}
+	
+	exec { 'etc_rc2.d_navision':
+		command => 'update-rc.d navision defaults 50',
+		creates => '/etc/rc2.d/S50-navision',
+		path => "/usr/sbin/:/usr/bin/:/bin/",
+		logoutput => true,
+		require => File ['/etc/init.d/navision'],
+	}
+
+	
+	file { '/opt/navision.jar':
+		ensure => 'present',
+		source => "puppet:///modules/navision/navision-launcher-real-${version}.jar",
+		owner => '0',
+		group => '0',
+		mode => '755',
+	}
+
+	file { '/etc/monit/conf.d/navision.conf':
+		ensure => 'present',
+		source => 'puppet:///modules/navision/monit-navision.conf',
+		owner => '0',
+		group => '0',
+		mode => '600',
+		require => [ Package ['monit'], File ['/etc/init.d/navision'] ],
+		notify => Service ['monit'],
+	}
+}
